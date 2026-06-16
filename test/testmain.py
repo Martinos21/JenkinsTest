98cc2568f75644d8508d77ce9f839c981ea5b96a
martin.tehnik <martin.tehnik@jablotron.cz>
2026-06-16 11:31:23 +0200

    init

---
+++ test/testmain.py
@@ -0,0 +1,13 @@
+from src.main import *
+
+def testAdd():
+    assert add(1,2) == 3
+
+def testSub():
+    assert sub(1,2) == -1
+
+def testMul():
+    assert mul(1,2) == 2
+
+def testDiv():
+    assert div(1,2) == 0.5
