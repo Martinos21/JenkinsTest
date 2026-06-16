71749b7542f25bf740112bf716bb1b3f69ddac62
martin.tehnik <martin.tehnik@jablotron.cz>
2026-06-16 14:12:20 +0200

    update test function naming convention for consistency

--- test/testmain.py
+++ test/testmain.py
@@ -1,13 +1,13 @@
 from src.main import *
 
-def testAdd():
+def test_Add():
     assert add(1,2) == 3
 
-def testSub():
+def test_Sub():
     assert sub(1,2) == -1
 
-def testMul():
+def test_Mul():
     assert mul(1,2) == 2
 
-def testDiv():
+def test_Div():
     assert div(1,2) == 0.5
